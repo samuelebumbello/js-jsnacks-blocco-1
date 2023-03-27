22adc10369d844a28d4c7b8baf4e2692977b117b
samuelebumbello <samuelebumbello17@gmail.com>
2023-03-27 22:31:10 +0200

    first commit

---
+++ JSnack-5/script.js
@@ -0,0 +1,25 @@
+/*
+
+JSnack 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array
+
+*/
+
+const oddNumbers = [];
+
+for(let i = 0; i < 6; i++){
+  let promptNumber = parseInt(prompt('Inserisci un numero:'));
+  if(promptNumber % 2 != 0){
+    console.log('numero dispari');
+    oddNumbers.push(promptNumber);
+    
+    console.log(promptNumber);
+    console.log('Numero dispari array', oddNumbers);
+}else{
+    console.log('numero pari');
+    console.log(promptNumber);
+    
+  }
+}
